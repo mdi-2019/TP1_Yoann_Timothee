e6a1c2d1e28081769bca35d85676dfb24345c97e
Timothée Schneider-Maunoury <47527408+PapaTimot@users.noreply.github.com>
2019-02-11 17:45:04 +0100

    Let the user choose the calculator mode(#1)
    before -> project with mathematics functions availables, but nothing implemented
    after  -> choice of calculator mode and call of the right functions
    
    This commit does the following:
    * main modified : implementation of the choice that call the apropriate function
    
    QA :
    * test choice of the calculator mode :
      -> normal mode
      -> scientifique mode
      -> wrong answer

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log('ça marche !!');
+const normal = require('./normal');
+const scientifique = require('./scientifique');
+
+console.log('\n**** Bienvenue dans ce super calculateur ! ****');
+let mode = "";
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var question = function(q) {
+    return new Promise( (res, rej) => {
+        rl.question( q, answer => {
+            res(answer);
+        })
+    });
+};
+
+(async function main() {
+    var answer;
+    while (answer != 1 && answer != 2) {
+        answer = await question("Choisissez un mode de calcul :\n  1 -> normal\n  2 -> scientifique\n");
+        if (answer != 1 && answer != 2) console.log("'" + answer + "' n'est pas une réponse valide, tapez '1' ou '2'");
+    }
+    if (answer == 1) mode = "normal";
+    else mode = "scientifique";
+    rl.close();
+    if (mode === "normal") normal.start();
+    else scientifique.start();
+})();
+
