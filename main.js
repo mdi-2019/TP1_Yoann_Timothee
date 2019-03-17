65f758c495d5eee0af6e3cb7830b53e145c8713f
Schneider-Maunoury Timothee <timothee.schneider-maunoury@etudiant.univ-rennes1.fr>
2019-03-17 17:24:07 +0100

    Ajout du calcul d'une puissance ou d'une racine carrée

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
 const normal = require('./normal');
 const scientifique = require('./scientifique');
+const readline = require('readline');
 
 console.log('\n**** Bienvenue dans ce super calculateur ! ****');
-let mode = "";
+console.log('Choisissez un mode de calcul :\n  1 -> normal\n  2 -> scientifique\n');
 
-const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-var question = function(q) {
-    return new Promise( (res, rej) => {
-        rl.question( q, answer => {
-            res(answer);
-        })
-    });
-};
-
-(async function main() {
-    var answer;
-    while (answer != 1 && answer != 2) {
-        answer = await question("Choisissez un mode de calcul :\n  1 -> normal\n  2 -> scientifique\n");
-        if (answer != 1 && answer != 2) console.log("'" + answer + "' n'est pas une réponse valide, tapez '1' ou '2'");
+function modeChoice(answer) {
+    if (answer != 1 && answer != 2) console.log("'" + answer + "' n'est pas une réponse valide, tapez '1' ou '2'");
+    else {
+        if (answer == 1) normal.start(rl);
+        else mode = scientifique.start(rl);
+        rl.removeListener('line', modeChoice);
     }
-    if (answer == 1) mode = "normal";
-    else mode = "scientifique";
-    rl.close();
-    if (mode === "normal") normal.start();
-    else scientifique.start();
-})();
+}
+
+rl.on('line', modeChoice);
+
+
+
+
+
+
 
--- normal.js
+++ normal.js
@@ -1,5 +1,34 @@
-module.exports = {
-    start : function(){
-        console.log("Mode choisi : normal\n");
-    }
+const mathjs = require('mathjs');
+
+start = function (rl) {
+    console.log('\nMode choisi : normal\n');
+    console.log("Entrez un calcul de la forme : 'op x [y]', avec op égal à 'pow' ou 'sqrt'");
+
+    rl.on('line', (a) => {
+        let answer = a.split(" ");
+        let res;
+        if (answer[0] == 'sqrt'){
+            try {
+                res = mathjs.sqrt(answer[1]);
+                console.log(res);
+            } catch (error) {
+                console.log("Le format du calcul entré n'est pas correct ...\n")
+            }
+        } 
+        else if (answer[0] == 'pow') {
+            try {
+                res = mathjs.pow(answer[1], answer[2]);
+                console.log(res);
+            } catch (error) {
+                console.log("Le format du calcul entré n'est pas correct ...\n")
+            }
+        }
+        else {
+            console.log("Le format du calcul entré n'est pas correct ...\n")
+        }
+    });
 }
+
+exports.start = start;
+
+
