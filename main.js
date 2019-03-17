af7d698950d25f670050d3908893d6832cb05131
Schneider-Maunoury Timothee <timothee.schneider-maunoury@etudiant.univ-rennes1.fr>
2019-03-17 18:03:47 +0100

    Calcul à partir du précédent résultat

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
-const normal = require('./normal');
-const scientifique = require('./scientifique');
-const readline = require('readline');
+const normal = require('./normal')
+const scientifique = require('./scientifique')
+const readline = require('readline')
 
-console.log('\n**** Bienvenue dans ce super calculateur ! ****');
-console.log('Choisissez un mode de calcul :\n  1 -> normal\n  2 -> scientifique\n');
+console.log('\n**** Bienvenue dans ce super calculateur ! ****')
+console.log('Choisissez un mode de calcul :\n  1 -> normal\n  2 -> scientifique\n')
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+})
+
+const previousResult = 0
 
 function modeChoice(answer) {
-    if (answer != 1 && answer != 2) console.log("'" + answer + "' n'est pas une réponse valide, tapez '1' ou '2'");
+    if (answer != 1 && answer != 2) console.log("'" + answer + "' n'est pas une réponse valide, tapez '1' ou '2'")
     else {
-        if (answer == 1) normal.start(rl);
-        else mode = scientifique.start(rl);
-        rl.removeListener('line', modeChoice);
+        if (answer == 1) normal.start()
+        else mode = scientifique.start()
+        rl.removeListener('line', modeChoice)
     }
 }
 
-rl.on('line', modeChoice);
+rl.on('line', modeChoice)
+
+exports.rl = rl
+exports.previousResult = previousResult
 
 
 
--- normal.js
+++ normal.js
@@ -1,34 +1,38 @@
-const mathjs = require('mathjs');
+const main = require('./main')
+const mathjs = require('mathjs')
 
-start = function (rl) {
-    console.log('\nMode choisi : normal\n');
-    console.log("Entrez un calcul de la forme : 'op x [y]', avec op égal à 'pow' ou 'sqrt'");
+start = function () {
+    console.log('\nMode choisi : normal\n')
+    console.log("Entrez un calcul de la forme : 'op x [y]', entrez 'rep' pour calculer à partir du résultat précédent.")
 
-    rl.on('line', (a) => {
-        let answer = a.split(" ");
-        let res;
+    main.rl.on('line', (a) => {
+        let answer = a.split(" ")
+        let res
         if (answer[0] == 'sqrt'){
             try {
-                res = mathjs.sqrt(answer[1]);
-                console.log(res);
+                res = mathjs.sqrt(answer[1] == 'rep' ? main.previousResult : answer [1])
+                console.log(res)
+                main.previousResult = res
             } catch (error) {
-                console.log("Le format du calcul entré n'est pas correct ...\n")
+                console.log("Le format du calcul entré n'est pas correct ...\nLes opérations traitées sont 'pow' et 'sqrt'.\n")
             }
         } 
         else if (answer[0] == 'pow') {
             try {
-                res = mathjs.pow(answer[1], answer[2]);
-                console.log(res);
+                res = mathjs.pow(answer[1] == 'rep' ? main.previousResult : answer [1],
+                                 answer[2] == 'rep' ? main.previousResult : answer [2])
+                console.log(res)
+                main.previousResult = res
             } catch (error) {
-                console.log("Le format du calcul entré n'est pas correct ...\n")
+                console.log("Le format du calcul entré n'est pas correct ...\nLes opérations traitées sont 'pow' et 'sqrt'.\n")
             }
         }
         else {
-            console.log("Le format du calcul entré n'est pas correct ...\n")
+            console.log("Le format du calcul entré n'est pas correct ...\nLes opérations traitées sont 'pow' et 'sqrt'.\n")
         }
-    });
+    })
 }
 
-exports.start = start;
+exports.start = start
 
 
